661a691c0f8a919a49e1dc3cd622894440a2b1ef
Rickson <rickson.simoes@hotmail.com>
2020-04-30 19:59:12 -0300

    :hammer: :wrench: TypeORM - Tabela de Agendamentos

---
+++ src/database/migrations/1588287375130-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1588287375130 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
