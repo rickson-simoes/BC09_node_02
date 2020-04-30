60192529e5fa93f79e05d884d6b49783c31d0d46
Rickson <rickson.simoes@hotmail.com>
2020-04-30 19:26:04 -0300

    :hammer: :wrench: TS - Configs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("✌️ Server started on port 3333");
+  console.log('✌️ Server started on port 3333');
 });
