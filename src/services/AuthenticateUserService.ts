a67f3524317baf892ab4a04d15e288664f889abc
Rickson <rickson.simoes@hotmail.com>
2020-05-06 17:14:00 -0300

    :hammer: Validacao e Autenticacao

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,47 @@
+import { compare } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+import { sign, verify } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface ServiceRequest {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: ServiceRequest): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, '4568c4219f0d98b57941fdf44f4ea739', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
