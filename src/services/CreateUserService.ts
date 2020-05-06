1b74580565e0be7723cf34fc5ad93c6f8c4999f1
Rickson <rickson.simoes@hotmail.com>
2020-05-05 21:00:44 -0300

    :hammer: Hash BCRYPT

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface ServiceRequest {
@@ -23,10 +25,12 @@ class CreateUserService {
       throw new Error('Email already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
