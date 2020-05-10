f9eda7b9ce781f8b88c56f1f3dd187e6d383e30f
Rickson <rickson.simoes@hotmail.com>
2020-05-09 23:33:27 -0300

    :hammer: Upload de Arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -21,8 +25,14 @@ usersRouter.post('/', async (req, res) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (req, res) => {
-  return res.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    console.log(req.file);
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
