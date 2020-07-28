6aac0dcf820ac935c065d9ba092292dff3aadca4
Rickson <rickson.simoes@hotmail.com>
2020-07-28 18:34:30 -0300

    :hammer: :wrench: Primeiro teste unitário - Ok

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('Should be able to create a new appointment', () => {
-    expect(3 + 3).toBe(6);
+  it('Should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
   });
 
   // it('Should not be able to create two appointments at the same time', () => {
