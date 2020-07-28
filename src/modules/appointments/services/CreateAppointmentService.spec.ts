0f9f5c6366d459372b667df03d5d5357d718b764
Rickson <rickson.simoes@hotmail.com>
2020-07-28 16:51:18 -0300

    :rotating_light: Primeiro teste de test :blush:

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,1 +1,11 @@
-test();
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', () => {
+    expect(3 + 3).toBe(6);
+  });
+
+  // it('Should not be able to create two appointments at the same time', () => {
+  //   expect(3 + 3).toBe(6);
+  // });
+});
