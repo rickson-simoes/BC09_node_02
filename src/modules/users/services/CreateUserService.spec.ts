0f80e1b79d9b168c26dfb6cdf083d536bf1aeed8
Rickson <rickson.simoes@hotmail.com>
2020-07-29 13:03:57 -0300

    :hammer: [2]Testes de Criacao de usuario - :rotating_light:

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('Should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      email: 'rickson.simoes@hotmail.com',
+      name: 'rickson',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user.name).toBe('rickson');
+  });
+
+  it('Should not be able to create a new user with an already registered email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      email: 'rickson.simoes@hotmail.com',
+      name: 'rickson',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        email: 'rickson.simoes@hotmail.com',
+        name: 'rickson',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
